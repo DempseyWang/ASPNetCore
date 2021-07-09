549d186f2ed70eebda83a912edfb965c2cb11a16
DempseyWang <bin.wang2@thoughtworks.com>
2021-07-09 16:52:01 +0800

    git@github.com:DempseyWang/ASPNetCore.git

---
+++ BasicASP.NETCoreMvc/BasicASP.NETCoreMvcAPP/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Claims;
+using BasicASP.NETMvc.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicASP.NETMvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (user == null || !"admin".Equals(user.UserName) || !"admin".Equals(user.PassWord))
+            {
+                ViewBag.Error = "UserName and PassWord is admin";
+                return View();
+            }
+
+            CreateAuthCookie(user.UserName);
+            AddValusToSession(user.UserName);
+            return RedirectToAction("Page");
+        }
+
+
+        //basic points 14 please make sure this action should be authed.
+        [Authorize]
+        public ActionResult Page()
+        {
+            // # homework 1 -- redirect to movies/index
+            //return View();
+            return RedirectToAction("movies/index");
+        }
+
+        private void CreateAuthCookie(string userName)
+        {
+            //basic points 16 please add param into Cookie 
+            // use cookie auth
+            var claims = new[] { new Claim("UserName", userName) };
+
+            var claimsIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+            ClaimsPrincipal user = new ClaimsPrincipal(claimsIdentity);
+            HttpContext.User = user;
+        }
+
+        private void AddValusToSession(string userName)
+        {
+            //basic points 17 Add param into Session and Seeeion key is "userName"
+            HttpContext.Session.SetString("userName", userName);
+        }
+    }
+}
---
+++ BasicASP.NETCoreMvc/BasicASP.NETCoreMvcAPP/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BasicASP.NETCoreMvcAPP
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";//名字随便起
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddControllersWithViews();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            //注册Cookie认证服务
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+
+                    builder => builder.AllowAnyOrigin()
+
+                    .WithMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS")
+
+                    );
+
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseSession();
+            //注意app.UseAuthentication方法一定要放在下面的app.UseMvc方法前面，否者后面就算调用HttpContext.SignInAsync进行用户登录后，使用
+            //HttpContext.User还是会显示用户没有登录，并且HttpContext.User.Claims读取不到登录用户的任何信息。
+            //这说明Asp.Net OWIN框架中MiddleWare的调用顺序会对系统功能产生很大的影响，各个MiddleWare的调用顺序一定不能反
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Auth}/{action=Login}/{id?}");
+                //endpoints.MapControllers();
+                //endpoints.MapRazorPages();
+            });
+        }
+    }
+}
